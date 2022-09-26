1a46a269ddb67499e359efc6387c18cb5124c64c
Richie <richardokonicha@gmail.com>
2022-09-26 11:34:37 +0100

    add telebot

--- app.py
+++ app.py
@@ -1,29 +1,68 @@
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+import telebot
+# from config import (wcapi, token, debug, webhook_url, bot, telebot)
+# from dotenv import load_dotenv
+import os
+
+# importdir.do("features", globals())
+webhook_url = ""
+token = "5410666315:AAEjKnX7OxCEvaJOZ8k5Bq17j8mIsR0vW1Y"
+debug = False
+
+# load_dotenv()
+
 app = Flask(__name__)
+
+bot = telebot.TeleBot(
+    token,
+    threaded=True
+)
+
+
+@bot.message_handler(commands=["start", "Start"])
+def start(message):
+    userid = message.from_user.id
+    bot.send_chat_action(userid, action='typing')
+    return bot.send_message(userid,  text="answer")
+
 
 
 @app.route('/')
 def index():
    print('Request for index page received')
-   return render_template('index.html')
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+   return ('This is a website.', 200, None)
 
 
-if __name__ == '__main__':
-   app.run()
+
+# @server.route('/', methods=['GET'])
+# def index():
+#     return ('This is a website.', 200, None)
+
+
+# @server.route('/monitor/order', methods=['POST'])
+# def new_order_hook():
+#     request_object = request.stream.read().decode("utf-8")
+#     return (request_object, 200, None)
+
+
+@app.route('/' + token, methods=['POST'])
+def getMessage():
+    request_object = request.stream.read().decode("utf-8")
+    update_to_json = [telebot.types.Update.de_json(request_object)]
+    bot.process_new_updates(update_to_json)
+    return "got Message bro"
+
+@app.route('/hook')
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(webhook_url + token)
+    return f"Webhook set to {webhook_url}"
+
+
+if debug == True:
+    bot.remove_webhook()
+    bot.polling()
+else:
+    if __name__ == "__main__":
+        app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8080)))
